cb48ab224feb3e0696d0abcf3382d7eafa15ff76
achraf59 <madaniachraf@hotmail.fr>
2016-01-11 15:23:44 +0100

    generer un Pecelable pour notre ObjetQuiz pour pouvoir les passer dans une intent une fois on choisi un ObjetQuiz parmi une liste de ObjetQuiz

--- app/src/main/java/premiereapplication/testautomation/quiz/objects/ObjetQuiz.java
+++ app/src/main/java/premiereapplication/testautomation/quiz/objects/ObjetQuiz.java
@@ -1,9 +1,13 @@
 package premiereapplication.testautomation.quiz.objects;
 
+import android.os.Parcel;
+import android.os.Parcelable;
+
+import java.util.ArrayList;
 import java.util.List;
 
 
-public class ObjetQuiz {
+public class ObjetQuiz implements Parcelable {
 
 public String nomQuiz;
 public int dureeQuiz;
@@ -15,6 +19,44 @@ public List<QuestionPropositionsReponses> listQuestionPropositionsReponses;
     this.dureeQuiz=dureeQuiz;
     this.listQuestionPropositionsReponses=listQuestionPropositionsReponses;
     }
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeString(this.nomQuiz);
+        dest.writeInt(this.dureeQuiz);
+        dest.writeList(this.listQuestionPropositionsReponses);
+    }
+
+    protected ObjetQuiz(Parcel in) {
+        this.nomQuiz = in.readString();
+        this.dureeQuiz = in.readInt();
+        this.listQuestionPropositionsReponses = new ArrayList<QuestionPropositionsReponses>();
+        in.readList(this.listQuestionPropositionsReponses, List.class.getClassLoader());
+    }
+
+    public static final Parcelable.Creator<ObjetQuiz> CREATOR = new Parcelable.Creator<ObjetQuiz>() {
+        public ObjetQuiz createFromParcel(Parcel source) {
+            return new ObjetQuiz(source);
+        }
+
+        public ObjetQuiz[] newArray(int size) {
+            return new ObjetQuiz[size];
+        }
+    };
+
+
+
+
+
+
+
+
+
 
 
 }
